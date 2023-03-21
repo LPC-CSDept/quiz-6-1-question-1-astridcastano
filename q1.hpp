bc34db799224642fa104461df0c13e4762c77fbc
astridcastano <115576792+astridcastano@users.noreply.github.com>
2023-03-21 10:13:21 -0700

    Update q1.hpp

--- q1.hpp
+++ q1.hpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void swapTwoValues(int n1, int n2)
+{
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void swapTwoValues(float f1, float f2)
+{
+    float temp = f1;
+    f1 = f2;
+    f2 = temp;
+}
+
+void getinput(int& num1, int& num2)
+{
+    cout << "Enter two integer values: ";
+    cin >> num1 >> num2;
+}
+
+void getinput(float& fnum1, float& fnum2)
+{
+    cout << "Enter two float values: ";
+    cin >> fnum1 >> fnum2;
+}
+
+
